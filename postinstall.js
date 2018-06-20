ddb260254b2ddd11b66aced5bfd2546750a6638f
Alexandru Macra <alexandru.macra@accesa.eu>
2018-06-20 09:23:21 +0300

    fixed the post install script

--- postinstall.js
+++ postinstall.js
@@ -1,140 +1,146 @@
 #!/usr/bin/env node
 'use strict';
 
-var pkg = require(require('path').resolve('./package.json'));
+var packageJsonRead = require(require('path').resolve('./package.json'))
+var helperStatus = packageJsonRead.helper.status;
 var inquirer = require('inquirer');
 const opn = require('opn');
 var exec = require('child_process').exec,
   child;
 
-if (pkg.collective) {
+function helperFunction() {
+
   console.log("\n");
-  console.log(`\u001b[96m\u001b[1mThank for installing ${pkg.name}!\u001b[96m\u001b[1m`);
+  console.log(`\u001b[96m\u001b[1mThank for installing ${packageJsonRead.name} version ${packageJsonRead.version}!\u001b[96m\u001b[1m`);
   console.log("\n");
   console.log(`                                                                                                                              
-  ##    ####   ####  ######  ####    ##      #####  ######  ####  #  ####  #    #     ####  #   #  ####  ##### ###### #    # 
- #  #  #    # #    # #      #       #  #     #    # #      #      # #    # ##   #    #       # #  #        #   #      ##  ## 
-#    # #      #      #####   ####  #    #    #    # #####   ####  # #      # #  #     ####    #    ####    #   #####  # ## # 
-###### #      #      #           # ######    #    # #           # # #  ### #  # #         #   #        #   #   #      #    # 
-#    # #    # #    # #      #    # #    #    #    # #      #    # # #    # #   ##    #    #   #   #    #   #   #      #    # 
-#    #  ####   ####  ######  ####  #    #    #####  ######  ####  #  ####  #    #     ####    #    ####    #   ###### #    #`);
+    ##    ####   ####  ######  ####    ##      #####  ######  ####  #  ####  #    #     ####  #   #  ####  ##### ###### #    # 
+   #  #  #    # #    # #      #       #  #     #    # #      #      # #    # ##   #    #       # #  #        #   #      ##  ## 
+  #    # #      #      #####   ####  #    #    #    # #####   ####  # #      # #  #     ####    #    ####    #   #####  # ## # 
+  ###### #      #      #           # ######    #    # #           # # #  ### #  # #         #   #        #   #   #      #    # 
+  #    # #    # #    # #      #    # #    #    #    # #      #    # # #    # #   ##    #    #   #   #    #   #   #      #    # 
+  #    #  ####   ####  ######  ####  #    #    #####  ######  ####  #  ####  #    #     ####    #    ####    #   ###### #    #`);
   console.log("\n\n");
-}
 
-const getReactQuestion = [
-  {
-    type: 'confirm',
-    name: 'design',
-    message: 'Would you like download the base design files?',
-    default: true
-  },
-  {
-    type: 'confirm',
-    name: 'yes',
-    message: 'Would you like to automatically integrate react (takes a few moments)?',
-    default: true
-  }
-];
-
-const startServerQuestion = [
-  {
-    type: 'confirm',
-    name: 'yes',
-    message: 'Would you like to start a local server and open the project right away?',
-    default: true
-  }
-];
-
-inquirer
-  .prompt(getReactQuestion)
-  .then(function (answers) {
-
-    if (answers.design === true) {
-      console.log("\n");
-      console.log(`\u001b[96m\u001b[1mDownloading the design files...\u001b[96m\u001b[1m`);
-
-      child = exec('git clone https://github.com/alex-macra/design',
-        function (error, stderr) {
-          if (error === null) {
-            console.log(`\u001b[96m\u001b[1mDesign files were downloaded with success.\u001b[96m\u001b[1m`);
-            console.log("\n");
-
-            if (answers.yes === true) {
-              console.log(`\u001b[96m\u001b[1mPlease wait for the react starter to install...\u001b[96m\u001b[1m`);
-            }
-          }
-
-          if (error !== null) {
-            console.log('exec error: ' + error);
-          }
-        });
+  const getReactQuestion = [
+    {
+      type: 'confirm',
+      name: 'design',
+      message: 'Would you like download the base design files?',
+      default: true
+    },
+    {
+      type: 'confirm',
+      name: 'yes',
+      message: 'Would you like to automatically integrate react (takes a few moments)?',
+      default: true
     }
+  ];
 
-    if (answers.yes === true) {
-      if (answers.design === false) {
+  const startServerQuestion = [
+    {
+      type: 'confirm',
+      name: 'yes',
+      message: 'Would you like to start a local server and open the project right away?',
+      default: true
+    }
+  ];
+
+  inquirer
+    .prompt(getReactQuestion)
+    .then(function (answers) {
+
+      if (answers.design === true) {
         console.log("\n");
-        console.log(`\u001b[96m\u001b[1mDownloading react starter pack...\u001b[96m\u001b[1m`);
+        console.log(`\u001b[96m\u001b[1mDownloading the design files...\u001b[96m\u001b[1m`);
+
+        child = exec('git clone https://github.com/alex-macra/design',
+          function (error, stderr) {
+            if (error === null) {
+              console.log(`\u001b[96m\u001b[1mDesign files were downloaded with success.\u001b[96m\u001b[1m`);
+              console.log("\n");
+
+              if (answers.yes === true) {
+                console.log(`\u001b[96m\u001b[1mPlease wait for the react starter to install...\u001b[96m\u001b[1m`);
+              }
+            }
+
+            if (error !== null) {
+              console.log('exec error: ' + error);
+            }
+          });
       }
 
-      child = exec('git clone https://github.com/alex-macra/react',
-        function (error, stderr) {
-          if (error === null) {
-            console.log(`\u001b[96m\u001b[1mReact starter pack has been downloaded with success.\u001b[96m\u001b[1m`);
-            console.log(`\u001b[96m\u001b[1mNext step, Installing the react dependencies. This can take a while on a slow speed internet.\u001b[96m\u001b[1m`);
-            
-            if (answers.design === false) {
-              console.log(`\u001b[96m\u001b[1mPlease wait...\u001b[96m\u001b[1m`);
+      if (answers.yes === true) {
+        if (answers.design === false) {
+          console.log("\n");
+          console.log(`\u001b[96m\u001b[1mDownloading react starter pack...\u001b[96m\u001b[1m`);
+        }
+
+        child = exec('git clone https://github.com/alex-macra/react',
+          function (error, stderr) {
+            if (error === null) {
+              console.log(`\u001b[96m\u001b[1mReact starter pack has been downloaded with success.\u001b[96m\u001b[1m`);
+              console.log(`\u001b[96m\u001b[1mNext step, Installing the react dependencies. This can take a while on a slow speed internet.\u001b[96m\u001b[1m`);
+
+              if (answers.design === false) {
+                console.log(`\u001b[96m\u001b[1mPlease wait...\u001b[96m\u001b[1m`);
+              }
+              console.log("\n");
             }
-            console.log("\n");
-          }
 
-          exec('cd react && npm install',
-            function (error, stderr) {
-              if (error == null) {
-                console.log(`\u001b[96m\u001b[1mReact starter pack was installed with success.\u001b[96m\u001b[1m`);
-              }
+            exec('cd react && npm install',
+              function (error, stderr) {
+                if (error == null) {
+                  console.log(`\u001b[96m\u001b[1mReact starter pack was installed with success.\u001b[96m\u001b[1m`);
+                }
 
-              inquirer
-                .prompt(startServerQuestion)
-                .then(function (answers) {
+                inquirer
+                  .prompt(startServerQuestion)
+                  .then(function (answers) {
 
-                  if (answers.yes === true) {
-                    if (error == null) {
-                      console.log(`\u001b[96m\u001b[1mStarting up the local server...\u001b[96m\u001b[1m`);
+                    if (answers.yes === true) {
+                      if (error == null) {
+                        console.log(`\u001b[96m\u001b[1mStarting up the local server...\u001b[96m\u001b[1m`);
+                      }
+
+                      exec('cd react && npm run start',
+                        function (error, stderr) {
+                          if (error !== null) {
+                            console.log('exec error: ' + error);
+                          }
+                        });
+
+                      if (error == null) {
+                        console.log(`\u001b[96m\u001b[1mPlease wait a couple of seconds...\u001b[96m\u001b[1m`);
+                        setTimeout(function () {
+                          opn('http://localhost:8888/');
+                          console.log(`\u001b[96m\u001b[1mAnd we are done!\u001b[96m\u001b[1m`);
+                        }, 8000);
+                      }
+
                     }
-
-                    exec('cd react && npm run start',
-                      function (error, stderr) {
-                        if (error !== null) {
-                          console.log('exec error: ' + error);
-                        }
-                      });
-
-                    if (error == null) {
-                      console.log(`\u001b[96m\u001b[1mPlease wait a couple of seconds...\u001b[96m\u001b[1m`);
-                      setTimeout(function () {
-                        opn('http://localhost:8888/');
-                        console.log(`\u001b[96m\u001b[1mAnd we are done!\u001b[96m\u001b[1m`);
-                      }, 8000);
+                    if (answers.yes === false) {
+                      console.log(`\u001b[96m\u001b[1mThat's it. Happy coding!\u001b[96m\u001b[1m`);
                     }
+                  })
 
-                  }
-                  if (answers.yes === false) {
-                    console.log(`\u001b[96m\u001b[1mThat's it. Happy coding!\u001b[96m\u001b[1m`);
-                  }
-                })
+                if (error !== null) {
+                  console.log('exec error: ' + error);
+                }
+              });
 
-              if (error !== null) {
-                console.log('exec error: ' + error);
-              }
-            });
+            if (error !== null) {
+              console.log('exec error: ' + error);
+            }
+          });
+      }
+      if (answers.yes === false) {
+        console.log(`\u001b[96m\u001b[1mThat's it. Happy coding!\u001b[96m\u001b[1m`);
+      }
+    })
+}
 
-          if (error !== null) {
-            console.log('exec error: ' + error);
-          }
-        });
-    }
-    if (answers.yes === false) {
-      console.log(`\u001b[96m\u001b[1mThat's it. Happy coding!\u001b[96m\u001b[1m`);
-    }
-  })
+if (helperStatus === "active") {
+  helperFunction();
+}
